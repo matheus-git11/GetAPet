6f131c6749e580089729ea050c735935d7756fc3
Matheus albuquerque <matheus.2020110153@unicap.br>
2023-07-27 02:45:23 -0300

    Formando container para vizualizacao de paginas

--- frontend/GetAPet/src/App.jsx
+++ frontend/GetAPet/src/App.jsx
@@ -9,16 +9,19 @@ import Home from "./components/pages/Home";
 //components
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import Container from './components/layout/Container'
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Container>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Container>
       <Footer />
     </Router>
   );
---
+++ frontend/GetAPet/src/components/layout/Container.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+import styles from './Container.module.css'
+
+function Container({children}) {
+  return (
+    <main className={styles.container}>
+        {children}
+    </main>
+  );
+}
+
+export default Container;
