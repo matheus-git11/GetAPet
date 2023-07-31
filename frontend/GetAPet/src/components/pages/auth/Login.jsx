da44fa02c30da89313f9cfc697d1a293d8d66bfb
Matheus albuquerque <matheus.2020110153@unicap.br>
2023-07-31 18:52:41 -0300

    Fazendo formulario de login

--- frontend/GetAPet/src/components/pages/auth/Login.jsx
+++ frontend/GetAPet/src/components/pages/auth/Login.jsx
@@ -1,7 +1,36 @@
+import { useState, useContext } from "react"
+import Input from "../../form/Input"
+import Styles from './Form.module.css'
+import { Context } from "../../../context/UserContext"
+import { Link } from "react-router-dom"
+
 function Login(){
+    function handleChange(e){}
+
     return(
-        <section>
+        <section className={Styles.form_container}>
             <h1>Login</h1>
+            <form >
+                <Input 
+                text='Email'
+                type="email"
+                name="email"
+                placeholder="digite seu e-mail"
+                handleOnChange={handleChange}
+                />
+                <Input 
+                text='Senha'
+                type="password"
+                name="password"
+                placeholder="digite seu e-mail"
+                handleOnChange={handleChange}
+                />
+                <input type="submit" value="Entrar"/>
+            </form>
+
+            <p>
+                Nao tem conta? <Link to="/register">Clique Aqui.</Link>
+            </p>
         </section>
     )
 }
