744e548588366c05e31385ed03e71e74fe42d2ab
Matheus albuquerque <matheus.2020110153@unicap.br>
2023-08-01 20:18:59 -0300

    Atualizando profile atraves do formulario

--- frontend/GetAPet/src/components/pages/User/Profile.jsx
+++ frontend/GetAPet/src/components/pages/User/Profile.jsx
@@ -1,82 +1,112 @@
-import { useState,useEffect } from 'react'
-import api from '../../../utils/api'
-import Input from '../../form/Input'
-import formStyles from '../Auth/Form.module.css'
-import styles from './Profile.module.css'
+import { useState, useEffect } from "react";
+import api from "../../../utils/api";
+import Input from "../../form/Input";
+import formStyles from "../Auth/Form.module.css";
+import styles from "./Profile.module.css";
+import useFlashMessage from "../../../hooks/useFlashMessage";
 
+function Profile() {
+  const [user, setUser] = useState({});
+  const [token] = useState(localStorage.getItem("token") || "");
+  const { setFlashMessage } = useFlashMessage();
 
-function Profile (){
-    const [user,setUser] = useState({})
-    const [token] = useState(localStorage.getItem('token') || '')
+  useEffect(() => {
+    api
+      .get("/users/checkuser", {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+        },
+      })
+      .then((response) => {
+        setUser(response.data);
+      });
+  }, [token]);
 
-    useEffect(() =>{
-        api.get('/users/checkuser',{
-            headers:{
-                Authorization: `Bearer ${JSON.parse(token)}`
-            }
-        }).then((response) =>{
-            setUser(response.data)
-        })
-    },[token])
+  function onFileChange(e) {
+    setUser({ ...user, [e.target.name]: e.target.files[0] });
+  }
+  function handleChange(e) {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    let msgType = "sucess";
+    const formData = new FormData();
+    await Object.keys(user).forEach((key) => {
+      formData.append(key, user[key]);
+    });
+    const data = await api
+      .patch(`/users/edit/${user._id}`, formData, {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        msgType = "error";
+        return error.response.data;
+      });
+    setFlashMessage(data.message, msgType);
+  }
 
-    function onFileChange(e){}
-    function handleChange(e){}
-
-    return(
-        <section>
-            <div className={styles.profile_header}>
-            <h1>Perfil</h1>
-            <p>Preview Imagem</p>
-            </div>
-            <form className={formStyles.form_container}>
-                <Input
-                    text="Imagem"
-                    type="file"
-                    name="image"
-                    handleOnChange={onFileChange}
-                />
-                <Input
-                    text="E-mail"
-                    type="email"
-                    name="email"
-                    placeholder="Digite o seu e-mail"
-                    handleOnChange={handleChange}
-                    value={user.email || ''}
-                />
-                <Input
-                    text="Nome"
-                    type="text"
-                    name="name"
-                    placeholder="Digite o seu e-nome"
-                    handleOnChange={handleChange}
-                    value={user.name || ''}
-                />
-                <Input
-                    text="Telefone"
-                    type="text"
-                    name="phone"
-                    placeholder="Digite o seu Telefone"
-                    handleOnChange={handleChange}
-                    value={user.phone || ''}
-                />
-                <Input
-                    text="Senha"
-                    type="password"
-                    name="password"
-                    placeholder="Digite a sua senha"
-                    handleOnChange={handleChange}
-                />
-                <Input
-                    text="Senha"
-                    type="password"
-                    name="confirmpassword"
-                    placeholder="Confime a sua senha"
-                    handleOnChange={handleChange}
-                />
-                <input type="submit" value='editar'/>
-            </form>
-        </section>
-    )
+  return (
+    <section>
+      <div className={styles.profile_header}>
+        <h1>Perfil</h1>
+        <p>Preview Imagem</p>
+      </div>
+      <form onSubmit={handleSubmit} className={formStyles.form_container}>
+        <Input
+          text="Imagem"
+          type="file"
+          name="image"
+          handleOnChange={onFileChange}
+        />
+        <Input
+          text="E-mail"
+          type="email"
+          name="email"
+          placeholder="Digite o seu e-mail"
+          handleOnChange={handleChange}
+          value={user.email || ""}
+        />
+        <Input
+          text="Nome"
+          type="text"
+          name="name"
+          placeholder="Digite o seu e-nome"
+          handleOnChange={handleChange}
+          value={user.name || ""}
+        />
+        <Input
+          text="Telefone"
+          type="text"
+          name="phone"
+          placeholder="Digite o seu Telefone"
+          handleOnChange={handleChange}
+          value={user.phone || ""}
+        />
+        <Input
+          text="Senha"
+          type="password"
+          name="password"
+          placeholder="Digite a sua senha"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Senha"
+          type="password"
+          name="confirmpassword"
+          placeholder="Confime a sua senha"
+          handleOnChange={handleChange}
+        />
+        <input type="submit" value="editar" />
+      </form>
+    </section>
+  );
 }
 
-export default Profile
+export default Profile;
