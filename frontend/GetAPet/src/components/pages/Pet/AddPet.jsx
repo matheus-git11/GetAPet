7541719d17607590c2c90e1a3c7a978ec4470988
Matheus albuquerque <matheus.2020110153@unicap.br>
2023-08-02 05:15:29 -0300

    Construindo Component Select

--- frontend/GetAPet/src/components/pages/Pet/AddPet.jsx
+++ frontend/GetAPet/src/components/pages/Pet/AddPet.jsx
@@ -1,7 +1,17 @@
+import { useState } from 'react'
+import api from '../../../utils/api'
+import styles from './AddPet.module.css'
+import useFlashMessage from '../../../hooks/useFlashMessage'
+import PetForm from './PetForm'
+
 function AddPet() {
   return (
-    <section>
-      <h1>AddPet</h1>
+    <section className={styles.addpet_header}>
+      <div>
+        <h1>Cadastre um Pet</h1>
+        <p>Depois ele ficara disponivel para adocao</p>
+      </div>
+      <PetForm btnText="Cadastrar Pet"/>
     </section>
   )
 }
