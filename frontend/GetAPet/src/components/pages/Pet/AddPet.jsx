24b5df051ef5d7592ea52e839ffb578e6f9f435f
Matheus albuquerque <matheus.2020110153@unicap.br>
2023-08-02 18:13:40 -0300

    Construindo funcao de registrar pet + Persistencia no BD

--- frontend/GetAPet/src/components/pages/Pet/AddPet.jsx
+++ frontend/GetAPet/src/components/pages/Pet/AddPet.jsx
@@ -1,17 +1,51 @@
 import { useState } from 'react'
+import { useNavigate } from "react-router-dom";
 import api from '../../../utils/api'
 import styles from './AddPet.module.css'
 import useFlashMessage from '../../../hooks/useFlashMessage'
 import PetForm from './PetForm'
 
 function AddPet() {
+  const [token] = useState(localStorage.getItem('token') || '')
+  const{setFlashMessage} = useFlashMessage()
+  const navigate = useNavigate()
+
+  async function registerPet(pet){
+    let msgType = 'sucess'
+    const formData = new FormData
+    await Object.keys(pet).forEach((key) =>{
+      if(key === 'images'){
+        for(let i = 0 ; i < pet[key].length; i++){
+          formData.append('images',pet[key][i])
+        }
+      }else{
+        formData.append(key,pet[key])
+      }
+    })
+
+    const data = await api.post('pets/create',formData,{
+       Authorization: `Bearer ${JSON.parse(token)}`,
+       'Content-Type' : 'multipart/form-data'
+    })
+    .then((response) =>{
+      return response.data
+    })
+    .catch((error)=>{
+      msgType = 'error'
+      return error.response.data
+    })
+      setFlashMessage(data.message,msgType)
+      msgType !== 'error' ? navigate('/pets/mypets') : ''
+  }
+
+
   return (
     <section className={styles.addpet_header}>
       <div>
         <h1>Cadastre um Pet</h1>
         <p>Depois ele ficara disponivel para adocao</p>
       </div>
-      <PetForm btnText="Cadastrar Pet"/>
+      <PetForm handleSubmit={registerPet} btnText="Cadastrar Pet"/>
     </section>
   )
 }
--- frontend/GetAPet/src/components/pages/Pet/PetForm.jsx
+++ frontend/GetAPet/src/components/pages/Pet/PetForm.jsx
@@ -20,7 +20,7 @@ function PetForm({ handleSubmit, petData, btnText }) {
   }
   function submit(e) {
     e.preventDefault();
-    console.log(pet);
+    handleSubmit(pet)
   }
 
   return (
