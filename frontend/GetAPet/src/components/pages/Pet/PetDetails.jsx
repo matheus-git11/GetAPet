e45a3e6776f07816b7ec30d7d66b66c03f812bd3
Matheus albuquerque <matheus.2020110153@unicap.br>
2023-08-06 21:27:39 -0300

    Processo de agendamento do pet

--- frontend/GetAPet/src/components/pages/Pet/PetDetails.jsx
+++ frontend/GetAPet/src/components/pages/Pet/PetDetails.jsx
@@ -1,13 +1,74 @@
-import { useEffect, useState } from 'react'
-import { useParams , Link} from 'react-router-dom'
-import api from '../../../utils/api'
-import styles from './PetDetails.module.css'
-import useFlashMessage from '../../../hooks/useFlashMessage'
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import api from "../../../utils/api";
+import styles from "./PetDetails.module.css";
+import useFlashMessage from "../../../hooks/useFlashMessage";
 
-function PetDetails(){
-    return(
-        <h1>Pagina de Pet</h1>
-    )
+function PetDetails() {
+  const [pet, setPet] = useState({});
+  const { id } = useParams();
+  const { setFlashMessage } = useFlashMessage();
+  const [token] = useState(localStorage.getItem("token") || "");
+
+  useEffect(() => {
+    api.get(`/pets/${id}`).then((response) => {
+      setPet(response.data.pet);
+    });
+  }, [id]);
+
+  async function schedule() {
+    let msgType = "sucess";
+    const data = await api
+      .patch(`pets/schedule/${pet._id}`, {
+        Authorization: `bearer ${JSON.parse(token)}`,
+      })
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        msgType = "error";
+        return error.response.data;
+      });
+
+    setFlashMessage(data.message, msgType);
+  }
+
+  return (
+    <>
+      {pet.name && (
+        <section className={styles.pet_details_container}>
+          <div className={styles.pet_details_header}>
+            <h1>Conhecendo o {pet.name}</h1>
+            <p>Se tiver interesse, marque uma visita para conhece-lo</p>
+          </div>
+          <div className={styles.pet_images}>
+            {pet.images.map((image, index) => (
+              <img
+                src={`http://localhost:5000/images/pets/${image}`}
+                alt={pet.name}
+                key={index}
+              />
+            ))}
+          </div>
+          <p>
+            <span className="bold">Peso:</span> {pet.weight} Kg
+          </p>
+          <p>
+            <span className="bold">Idade:</span> {pet.age} Ano
+          </p>
+
+          {token ? (
+            <button onClick={schedule}>Solicitar uma Visita</button>
+          ) : (
+            <p>
+              Voce precisa <Link to="/register">Criar uma conta</Link> para
+              solicitar a visita
+            </p>
+          )}
+        </section>
+      )}
+    </>
+  );
 }
 
-export default PetDetails
+export default PetDetails;
